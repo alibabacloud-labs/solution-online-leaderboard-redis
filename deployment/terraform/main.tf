00898dd99068dc318d3fbcb8ffa03cc7accee80d
Xiang Zhou <junlan.zx@alibaba-inc.com>
2021-02-26 17:59:03 +0800

    Add terraform deployment for redis and ecs.

---
+++ deployment/terraform/main.tf
@@ -0,0 +1,96 @@
+provider "alicloud" {
+#   access_key = "${var.access_key}"
+#   secret_key = "${var.secret_key}"
+  region     = "ap-southeast-1"
+}
+
+variable "name" {
+  default = "auto_provisioning_group"
+}
+
+# Create a new ECS instance for a VPC
+resource "alicloud_security_group" "group" {
+  name        = "sg_solution_online_leaderboard"
+  description = "foo"
+  vpc_id      = alicloud_vpc.vpc.id
+}
+
+# resource "alicloud_kms_key" "key" {
+#   description            = "Hello KMS"
+#   pending_window_in_days = "7"
+#   key_state              = "Enabled"
+# }
+
+# Create a new ECS instance for VPC
+resource "alicloud_vpc" "vpc" {
+  name       = var.name
+  cidr_block = "172.16.0.0/16"
+}
+
+resource "alicloud_vswitch" "vswitch" {
+  vpc_id            = alicloud_vpc.vpc.id
+  cidr_block        = "172.16.0.0/24"
+  availability_zone = data.alicloud_zones.default.zones[0].id
+  name              = var.name
+}
+
+# resource "alicloud_slb" "slb" {
+#   name       = "test-slb-tf"
+#   vswitch_id = alicloud_vswitch.vswitch.id
+# }
+
+resource "alicloud_instance" "instance" {
+  # cn-beijing
+  # availability_zone = "cn-beijing-b"
+  security_groups   = alicloud_security_group.group.*.id
+
+  # series III
+  instance_type              = "ecs.n4.large"
+  system_disk_category       = "cloud_efficiency"
+  system_disk_name           = "test_foo_system_disk_name"
+  system_disk_description    = "test_foo_system_disk_description"
+  image_id                   = "ubuntu_18_04_64_20G_alibase_20190624.vhd"
+  instance_name              = "test_foo"
+  vswitch_id                 = alicloud_vswitch.vswitch.id
+  internet_max_bandwidth_out = 10
+  data_disks {
+    name        = "disk2"
+    size        = 20
+    category    = "cloud_efficiency"
+    description = "disk2"
+    # encrypted   = true
+    # kms_key_id  = alicloud_kms_key.key.id
+  }
+}
+
+######## Redis
+variable "redis_name" {
+  default = "redis"
+}
+
+variable "creation" {
+  default = "KVStore"
+}
+
+data "alicloud_zones" "default" {
+  available_resource_creation = var.creation
+}
+
+resource "alicloud_kvstore_instance" "example" {
+  db_instance_name      = "tf-test-basic"
+  vswitch_id            = alicloud_vswitch.vswitch.id
+  security_group_id     = alicloud_security_group.group.id
+  instance_type         = "Redis"
+  engine_version        = "4.0"
+  config = {
+    appendonly = "yes",
+    lazyfree-lazy-eviction = "yes",
+  }
+  tags = {
+    Created = "TF",
+    For = "Test",
+  }
+  resource_group_id     = "rg-123456"
+  zone_id               = data.alicloud_zones.default.zones[0].id
+  instance_class        = "redis.master.micro.default"
+}
